4dcc3e1e3c4867c612180f877757dd13aa702dd6
lucasrls98 <lucasrls98@gmail.com>
2023-02-17 12:47:16 -0300

    tentativa1-sql

--- telaDeCadastro-master/telaDeCadastro/MainWindow.xaml.cs
+++ telaDeCadastro-master/telaDeCadastro/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Data.SqlClient;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -74,5 +75,34 @@ namespace telaDeCadastro
             }
         }
 
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=AVELL\\SQLEXPRESS;Initial Catalog=dadosCadastro;Integrated security=True");
+
+            try
+            {
+                conn.Open();
+
+                foreach (var item in lstEnderecos.Items)
+                {
+                    string endereco = item.ToString();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO tblUser (endereco) VALUES (@endereco)", conn);
+                    cmd.Parameters.AddWithValue("@endereco", txtEndereco.Text);
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Dados salvos com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao salvar dados: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        
     }
 }
